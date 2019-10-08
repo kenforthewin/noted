89555283515e4cbf4bd909d8a30f618370ceb286
Kenny <bergquist.kenneth@gmail.com>
2019-10-07 21:33:57 -0400

    markdown

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let monacoEditor
+let mainWindow;
+let monacoEditor;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    darkTheme: true,
-    backgroundColor: '#1e1e1e',
-    title: 'Noted',
-    titleBarStyle: 'hidden',
+    backgroundColor: "#1e1e1e",
+    title: "Noted",
+    titleBarStyle: "hidden",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true
     }
-  })
+  });
 
-  mainWindow.setSheetOffset(22)
+  mainWindow.setSheetOffset(22);
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  app.quit()
-})
+app.on("window-all-closed", function() {
+  app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+  if (mainWindow === null) createWindow();
+});
--- renderer.js
+++ renderer.js
@@ -2,46 +2,93 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-let monacoEditor
+const hljs = require("highlight.js");
+var md = require("markdown-it")({
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return (
+          '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          "</code></pre>"
+        );
+      } catch (__) {}
+    }
+
+    return (
+      '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + "</code></pre>"
+    );
+  }
+});
+
+let monacoEditor;
+const mdElement = document.getElementById("md-container");
+
+let str =
+  "# Abc\n\nHi\n\n# Def\n\n1. what\n1. ok\n\n# Ghi\n\n1. ok\n1. Why tho\n\n# What\n\nwhy\n\n# Where\n\n ok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sem nisi, finibus sit amet dolor vel, faucibus viverra dui. Donec pulvinar orci nibh, sed dictum quam fringilla ut. Vivamus pulvinar, quam vel rutrum feugiat, augue magna hendrerit tellus, mattis luctus nulla lorem non tortor. Proin quis molestie justo. Sed suscipit pretium ultrices. Mauris ac nisl vulputate elit fermentum sagittis a id risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;";
+const tabDiv = document.createElement("div");
+tabDiv.className = "tab";
+const monacoContainer = document.getElementById("container");
 
 (function() {
-  const path = require('path')
-  const amdLoader = require('monaco-editor/min/vs/loader');
+  const path = require("path");
+  const amdLoader = require("monaco-editor/min/vs/loader");
   const amdRequire = amdLoader.require;
   function uriFromPath(_path) {
-    var pathName = path.resolve(_path).replace(/\\/g, '/');
-    if (pathName.length > 0 && pathName.charAt(0) !== '/') {
-      pathName = '/' + pathName;
+    var pathName = path.resolve(_path).replace(/\\/g, "/");
+    if (pathName.length > 0 && pathName.charAt(0) !== "/") {
+      pathName = "/" + pathName;
     }
-    return encodeURI('file://' + pathName);
+    return encodeURI("file://" + pathName);
   }
   amdRequire.config({
-    baseUrl: uriFromPath(path.join(__dirname, './node_modules/monaco-editor/min'))
+    baseUrl: uriFromPath(
+      path.join(__dirname, "./node_modules/monaco-editor/min")
+    )
   });
   // workaround monaco-css not understanding the environment
   self.module = undefined;
-  amdRequire(['vs/editor/editor.main'], function() {
-    monacoEditor = monaco.editor.create(document.getElementById('container'), {
-      value: [
-        '# Noted'
-      ].join('\n'),
-      language: 'markdown',
+  amdRequire(["vs/editor/editor.main"], function() {
+    monacoEditor = monaco.editor.create(monacoContainer, {
+      value: str,
+      language: "markdown",
       quickSuggestions: false,
       codeLens: false,
-      wordWrap: 'on',
-      theme: 'vs-dark'
+      wordWrap: "on",
+      theme: "vs-dark"
     });
 
-    monacoEditor.onDidChangeModelContent(onDidChangeModelContent)
+    monacoEditor.onDidChangeModelContent(onDidChangeModelContent);
+    monacoEditor.onKeyDown(onMonacoKeyDown);
   });
-})()
+})();
 
-window.addEventListener('resize', function() {
-  if (!monacoEditor) return
+function onMonacoKeyDown(e) {
+  if (e.keyCode === 9) {
+    mdElement.className = "md-container";
+    monacoContainer.className = "container hidden";
+    mdElement.innerHTML = md.render(str);
+  }
+}
 
-  monacoEditor.layout()
-})
+window.addEventListener("resize", function() {
+  if (!monacoEditor) return;
+
+  monacoEditor.layout();
+});
 
 function onDidChangeModelContent(e) {
-  console.log(monacoEditor.getValue())
+  str = monacoEditor.getValue();
 }
+
+const tabs = document.getElementById("tabs");
+for (let i = 0; i < 15; i++) {
+  tabs.append(tabDiv.cloneNode());
+}
+
+mdElement.innerHTML = md.render(str);
+mdElement.onclick = function(e) {
+  mdElement.className = "md-container hidden";
+  monacoContainer.className = "container";
+  monacoEditor.layout();
+};
