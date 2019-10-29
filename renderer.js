b758a2f7611707594d332504edb293819a281899
Kenny <bergquist.kenneth@gmail.com>
2019-10-28 20:04:35 -0400

    save file

--- renderer.js
+++ renderer.js
@@ -11,12 +11,10 @@ let editMode = false;
 const tabDiv = document.createElement("div");
 tabDiv.className = "tab";
 const monacoContainer = document.getElementById("container");
-const mainContainer = document.getElementById("main");
 const path = require("path");
 const amdLoader = require("monaco-editor/min/vs/loader");
 const amdRequire = amdLoader.require;
 const Nav = require("./src/models/Nav");
-const Note = require("./src/models/Note");
 amdRequire.config({
   baseUrl: uriFromPath(path.join(__dirname, "./node_modules/monaco-editor/min"))
 });
@@ -24,7 +22,7 @@ amdRequire.config({
 const nav = new Nav();
 // workaround monaco-css not understanding the environment
 self.module = undefined;
-amdRequire(["vs/editor/editor.main"], async function() {
+amdRequire(["vs/editor/editor.main"], function() {
   monacoEditor = monaco.editor.create(monacoContainer, {
     language: "markdown",
     quickSuggestions: false,
@@ -35,49 +33,55 @@ amdRequire(["vs/editor/editor.main"], async function() {
 
   monacoEditor.onDidChangeModelContent(onDidChangeModelContent);
   // monacoEditor.onKeyDown(onMonacoKeyDown);
-});
 
-document.addEventListener("keydown", onMonacoKeyDown);
+  let note;
+  nav.setup().then(async function() {
+    note = nav.getCurrentNote();
+    const el = await note.renderMD();
+    mdElement.innerHTML = el;
 
-async function onMonacoKeyDown(e) {
-  if (editMode && e.keyCode === 27 /*esc*/) {
-    switchToMDMode();
-  } else if (!editMode && e.keyCode === 73 /*i*/) {
-    switchToEditMode();
+    monacoEditor.getModel().setValue(note.body);
+  });
+
+  function onDidChangeModelContent(e) {
+    if (note) {
+      note.updateNote(monacoEditor.getValue());
+    }
   }
-}
 
-window.addEventListener("resize", function() {
-  if (!monacoEditor) return;
+  async function switchToMDMode(e) {
+    editMode = false;
+    mdElement.className = "md-container";
+    monacoContainer.className = "container hidden";
+    mdElement.innerHTML = await note.renderMD();
+  }
 
-  monacoEditor.layout();
+  window.addEventListener("resize", function() {
+    if (!monacoEditor) return;
+
+    monacoEditor.layout();
+  });
+
+  const tabs = document.getElementById("tabs");
+  for (let i = 0; i < 15; i++) {
+    tabs.append(tabDiv.cloneNode());
+  }
+
+  function switchToEditMode(e) {
+    editMode = true;
+    mdElement.className = "md-container hidden";
+    monacoContainer.className = "container";
+    monacoEditor.layout();
+  }
+  mdElement.ondblclick = switchToEditMode;
+
+  document.addEventListener("keydown", onMonacoKeyDown);
+
+  async function onMonacoKeyDown(e) {
+    if (editMode && e.keyCode === 27 /*esc*/) {
+      switchToMDMode();
+    } else if (!editMode && e.keyCode === 73 /*i*/) {
+      switchToEditMode();
+    }
+  }
 });
-
-function onDidChangeModelContent(e) {
-  // str = monacoEditor.getValue();
-}
-
-const tabs = document.getElementById("tabs");
-for (let i = 0; i < 15; i++) {
-  tabs.append(tabDiv.cloneNode());
-}
-
-nav.setup().then(function() {
-  const note = nav.getCurrentNote();
-  note.renderMD().then(el => (mdElement.innerHTML = el));
-});
-
-async function switchToMDMode(e) {
-  editMode = false;
-  mdElement.className = "md-container";
-  monacoContainer.className = "container hidden";
-  mdElement.innerHTML = await note.renderMD();
-}
-
-function switchToEditMode(e) {
-  editMode = true;
-  mdElement.className = "md-container hidden";
-  monacoContainer.className = "container";
-  monacoEditor.layout();
-}
-mdElement.ondblclick = switchToEditMode;
--- src/models/Note.js
+++ src/models/Note.js
@@ -1,12 +1,16 @@
 const { md } = require("../utils/md");
 const fs = require("fs").promises;
 
+const WAIT_INTERVAL = 200;
+
 class Note {
   constructor(file) {
     this.file = file;
 
     this.fileContents = this.fileContents.bind(this);
     this.readFile = this.readFile.bind(this);
+    this.writeFile = this.writeFile.bind(this);
+    this.updateNote = this.updateNote.bind(this);
   }
 
   async fileContents(refresh = false) {
@@ -21,8 +25,18 @@ class Note {
     this.body = await fs.readFile(this.file, "utf8");
   }
 
+  async writeFile() {
+    await fs.writeFile(this.file, this.body, { encoding: "utf8" });
+  }
+
   async renderMD() {
     return md.render(await this.fileContents(true));
+  }
+
+  updateNote(value) {
+    this.body = value;
+    clearTimeout(this.timer);
+    this.timer = setTimeout(this.writeFile, WAIT_INTERVAL);
   }
 }
 
